5c36cbf1b97e6f6ba9d904deda26b2bd16b2acde
Tim Roth <timdroth@gmail.com>
2021-11-21 00:28:30 -0800

    change validation paramters

--- ParksAPI/Models/Park.cs
+++ ParksAPI/Models/Park.cs
@@ -5,15 +5,11 @@ namespace ParksAPI.Models
     public class Park
     {
         public int ParkId { get; set; }
-        [Required]
         [StringLength(40)]
         public string Name{ get; set; }
-        [Required]
         [StringLength(40)]
         public string Location{ get; set; }
-        [Required]
         public bool National { get; set; }
-        [Required]
         public bool State { get; set; }
     }
 }
